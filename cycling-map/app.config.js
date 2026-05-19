395876fad5331a650c96464cea2adc4a057d5c36
Elimurray <eli.murr04@gmail.com>
2026-05-19 19:22:09 +1200

    remove api

---
+++ cycling-map/app.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  expo: {
+    name: "cycling-map",
+    slug: "cycling-map",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: "com.elimurray.cyclingmap",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+  },
+};
